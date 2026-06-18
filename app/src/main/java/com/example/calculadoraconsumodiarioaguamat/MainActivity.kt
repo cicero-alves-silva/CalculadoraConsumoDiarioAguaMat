2706a6d8603e09baf69f659f0d61ed8d4858de31
Cicero <cicero.alves@ifpb.edu.br>
2026-06-18 13:03:26 -0300

    Aula 18/06/2026

--- app/src/main/java/com/example/calculadoraconsumodiarioaguamat/MainActivity.kt
+++ app/src/main/java/com/example/calculadoraconsumodiarioaguamat/MainActivity.kt
@@ -2,10 +2,21 @@ package com.example.calculadoraconsumodiarioaguamat
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.example.calculadoraconsumodiarioaguamat.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        val binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.btCalcular.setOnClickListener { view ->
+            val quilos = binding.etPeso.text.toString().replace(",", ".").toDouble()
+            val litros = quilos * 0.035
+            val litrosFormatado = "%.3f".format(litros).replace(".", ",")
+            binding.tvResultado.text = "Você precisa ingerir " +
+                    "$litrosFormatado litro(s) de água por dia."
+        }
     }
 }
